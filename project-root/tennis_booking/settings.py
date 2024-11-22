d99e8595ac15bc61f0f1200589dd8e289c9d0c64
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-11-22 21:51:06 +0000

    Add: Set up MySQL connection

--- project-root/tennis_booking/settings.py
+++ project-root/tennis_booking/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'tennis_booking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
