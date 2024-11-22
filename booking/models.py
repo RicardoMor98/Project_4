46d9f7862a07eb50e67129d212047db72c96262e
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-11-22 21:54:15 +0000

    Add: defining models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Lesson(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    coach_name = models.CharField(max_length=100)
+    max_guests = models.IntegerField()
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    num_guests = models.IntegerField()
