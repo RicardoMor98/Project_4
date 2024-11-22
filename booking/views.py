a359862aadc836e8c09e7fb5e415dd59f8607e40
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-11-22 21:55:52 +0000

    Add. basic views

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Lesson
 
 # Create your views here.
+
+def home(request):
+    lessons = Lesson.objects.all()
+    return render(request, 'index.html', {'lessons': lessons})
